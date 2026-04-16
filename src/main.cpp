d09672faafc5d17de6c4233fc6896d74c08e628f
Алексей Дидык <adidyk110@gmail.com>
2026-04-16 15:17:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+using namespace std;
 
-    /* Initialize the library */
+int main(void) { GLFWwindow* window;
+/* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+/* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -16,21 +22,126 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+/* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+
+    auto vertexShaderCode = R"(
+    #version 330 core
+
+    in vec4 aPos;
+    in vec3 aColor;
+
+    out vec3 color;
+
+    void main() {
+        gl_Position = aPos;
+        color = aColor;
+    }
+)";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertexShader);
+
+    auto fragmentShaderCode = R"(
+    #version 330 core
+
+    in vec3 color;
+
+    out vec4 FragColor;
+
+    void main() {
+        FragColor = vec4(color, 1.0);
+}
+)";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragmentShader);
+
+// Програма з шейдерів
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,       0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,
+            0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f,      0.0f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,
+            -0.5f, 0.5f, 0.0f,        0.0f, 1.0f, 0.0f,
+    };
+
+
+
+
+    GLuint VBO;
+    GLuint VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1,&VBO);
+
+// робимо VAO поточним (він починає записувати налаштування
+    glBindVertexArray(VAO);
+
+// 2. Робимо VBO поточним буфером (GL_ARRAY_BUFFER означає буфер для вершин)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+// 3. Копіюємо наш масив vertices у пам'ять відеокарти
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            6* sizeof(float),
+            (void*)0
+
+    );
+    glEnableVertexAttribArray(0);
+
+
+    glVertexAttribPointer(1,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          6 * sizeof(float),
+                          (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+
+/* Loop until the user closes the window */
+    do
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
-    }
+    } while (!glfwWindowShouldClose(window));
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
